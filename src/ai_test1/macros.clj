3738dfeab8d2d43e73d73d2ac2b8471987f552db
htk2014 <budoutou@ezweb.ne.jp>
2014-09-23 17:46:35 +0900

    Update macros.clj

--- src/ai_test1/macros.clj
+++ src/ai_test1/macros.clj
@@ -19,7 +19,6 @@
       `(declare ~next-bh))))
 
 (defmacro my-cond
-  "For make-cond"
   [& clauses]
   (when clauses
     (list 'if (first (first clauses))
@@ -28,11 +27,6 @@
             (throw (IllegalArgumentException.
                     "cond requires an even number of forms")))
           (cons 'macros.core/my-cond (next clauses)))))
-
-(comment 
-  (my-cond [(= 1 3) 3]
-           [(= 1 2) 2]
-           [(= 1 1) 1]))
 
 (defn make-cond [state-var multi-cnd]
   `(my-cond
@@ -68,3 +62,94 @@
        (atom ~(symbol (:name (first behaviors)))))
      (defn ~(symbol name) ~args-vec
        ((deref ~(symbol (str name "-state"))) ~@args-vec))))
+
+(defaction obj :up-down []
+  {:bh printlnx})
+
+:up {:bhavior (fn []
+                 (mac/-= (-> EnemyCube .-position .-z) mv-distance)
+                 (reset! test-count (+ @test-count 1))
+                 (reset! up-count (+ @up-count 1)))
+     :end-cnd #(>= @test-count 20)
+     :next-bh "down"}
+:down {:behavior (fn []
+                   (mac/+= (-> EnemyCube .-position .-z) mv-distance)
+                   (reset! test-count (- @test-count 1)))
+       :end-cnd #(<= @test-count 0) :next-bh "left"}
+:left {:behavior (fn []
+                   (mac/-= (-> EnemyCube .-position .-x) mv-distance)
+                   (reset! test-count (- @test-count 1)))
+       :multi-cnd [{:end-cnd #(>= @up-count 100) :next-bh "down"}
+                   {:end-cnd #(<= @test-count -20) :next-bh "up"}]}
+
+(defmacro let-map
+   "Equivalent of (let [a 5 b (+ a 5)] {:a a :b b})."
+   [kvs]
+   (let [keys (keys (apply hash-map kvs))
+         keyword-symbols (mapcat #(vector (keyword (str %)) %) keys)]
+   `(let [~@kvs]
+      (hash-map ~@keyword-symbols))))
+
+(let-map [count (atom 0)
+          count-up (fn [] (reset! count (+ @count 1)))
+          count-down (fn [] (reset! count (- @count 1)))
+          up   {:behavior (fn [] 
+                           (count-up)
+                           (println "up"))
+                :end-cnd #(>= @count 10) :next-state :down}
+          down {:behavior (fn [] 
+                           (count-down)
+                           (println "down"))
+                :end-cnd #(< @count 0) :next-state :up}
+          state (atom up)
+          action (fn [this]
+                   (let [bhv (:behavior @state)
+                         end-cnd (:end-cnd @state)
+                         next-state ((:next-state @state) this)]
+                     (bhv)
+                     (if (end-cnd)
+                       (reset! state next-state))))])
+
+(let-map [count (atom 0)
+          count-all (atom 0)
+          count-up (fn [] 
+                     (reset! count (+ @count 1))
+                     (reset! count-all (+ @count-all 1)))
+          count-down (fn [] 
+                       (reset! count (- @count 1))
+                       (reset! count-all (+ @count-all 1)))
+          up   {:behavior (fn [] 
+                           (count-up)
+                           (println "up"))
+                :end-cnd #(>= @count 10) :next-state :down}
+          down {:behavior (fn [] 
+                           (count-down)
+                           (println "down"))
+                :multi-cnd  [{:end-cnd #(< @count 0) :next-state :up}
+                             {:end-cnd #(>= @count-all 30) :next-state :strange}]}
+          strange {:behavior (fn [] (println "strange"))
+                   :end-cnd (fn [] nil)}
+          state (atom up)
+          action (fn [this]
+                   (let [bhv (:behavior @state)
+                         end-cnd (:end-cnd @state)
+                         next-state (if (:next-state @state) ((:next-state @state) this))
+                         multi-cnd (:multi-cnd @state)]
+                     (bhv)
+                     ;update state
+                     (if multi-cnd
+                       (loop [cnd-vec multi-cnd]
+                         (println cnd-vec)
+                          (if (not (first cnd-vec))
+                            nil
+                            (let [cnd (first cnd-vec)
+                                  end-cnd (:end-cnd cnd)
+                                  next-state ((:next-state cnd) this)]
+                              (if (end-cnd)
+                                (reset! state next-state))
+                              (recur (rest cnd-vec)))))
+                       (if (end-cnd)
+                         (reset! state next-state)))))])
+
+(defn action [obj]
+  ((:action obj) obj))
