036d0afcc654ef805206b5777ea39d4f45c7d182
htk2014 <budoutou@ezweb.ne.jp>
2014-09-28 14:50:41 +0900

    Update util.cljs

--- src/ai_test1/util.cljs
+++ src/ai_test1/util.cljs
@@ -17,6 +17,9 @@
 (defn get-pos-z [obj]
   (-> obj .-position .-z))
 
+(defn get-rot-y [obj]
+  (-> obj .-rotation .-y))
+
 (defn line-length [v1 v2]
   (if (> v1 v2)
     (- v1 v2)
@@ -33,20 +36,43 @@
         pw2 (pow v2 2)]
     (sqrt (+ pw1 pw2))))
 
-(defn baz [main-o target-o]
+(comment(defn baz [main-o target-o]
   "return rotate value for look at target"
   (let [main-x (get-pos-x main-o)
         main-z (get-pos-z main-o)
         target-x (get-pos-x target-o)
         target-z (get-pos-z target-o)
-        other-x main-x
-        other-z target-z
-        line-a (line-length target-x other-x)
-        line-b (line-length main-z other-z)
+        other-x (if (> main-z target-z) target-x main-x)
+        other-z (if (> main-z target-z) main-z target-z)
+        line-a (if (> main-z target-z)
+                 (line-length target-z other-z)
+                 (line-length target-x other-x))
+        line-b (if (> main-z target-z) 
+                 (line-length main-x other-x)
+                 (line-length main-z other-z))
         line-c (pitago line-a line-b)
         pow-a (pow line-a 2)
         pow-b (pow line-b 2)
         pow-c (pow line-c 2)]
     (acos (/ (- (+ pow-b pow-c) pow-a) (* 2 line-b line-c)))))
+)
 
-
+(defn sol-rot [main-o target-o]
+  "return rotate value for look at target"
+  (let [main-x (get-pos-x main-o)
+        main-z (get-pos-z main-o)
+        target-x (get-pos-x target-o)
+        target-z (get-pos-z target-o)
+        other-x (if (> main-z target-z) target-x main-x)
+        other-z (if (> main-z target-z) main-z target-z)
+        line-a (if (> main-z target-z)
+                 (line-length target-z other-z)
+                 (line-length target-x other-x))
+        line-b (if (> main-z target-z) 
+                 (line-length main-x other-x)
+                 (line-length main-z other-z))
+        line-c (pitago line-a line-b)
+        pow-a (pow line-a 2)
+        pow-b (pow line-b 2)
+        pow-c (pow line-c 2)]
+    (acos (/ (- (+ pow-b pow-c) pow-a) (* 2 line-b line-c)))))
